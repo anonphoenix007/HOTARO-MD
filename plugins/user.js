5dc26e740d86e5abc165f9b5b4def3279aa76293
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-11-23 10:53:16 +0100

    Update user.js

--- plugins/user.js
+++ plugins/user.js
@@ -290,3 +290,31 @@ command(
     message.reply(`*Uptime:* ${secondsToDHMS(process.uptime())}\n\n☬ ʜᴏᴛᴀʀᴏ-ᴍᴅ ☬`);
   }
 );
+
+
+
+command(
+  {
+    pattern: "ss ?(.*)",
+    fromMe: true,
+    desc: "Screenshots a site",
+    type: "misc",
+  },
+  async (message, match) => {
+    if (!match) {
+      return await message.sendMessage("*Please provide a URL to screenshot.*");
+    }
+
+    const screenshotUrl = `https://api.giftedtech.my.id/api/tools/vurl?apikey=gifted&url=${encodeURIComponent(match)}`;
+
+    await message.client.sendMessage(
+      message.jid,
+      {
+        image: { url: screenshotUrl },
+        mimetype: "image/jpeg",
+        caption: `\n> screenshot of ${(match)} generated successfully `,
+      },
+      { quoted: message }
+    );
+  }
+);
